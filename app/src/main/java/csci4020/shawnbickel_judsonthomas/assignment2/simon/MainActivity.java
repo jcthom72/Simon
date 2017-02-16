4583061f394c4ac950951687e0717fb957e6f984
student456773 <sbickel201@gmail.com>
2017-02-15 18:03:33 -0600

    added some mp3 files for game sound; added minor changes to layout

--- app/src/main/java/csci4020/shawnbickel_judsonthomas/assignment2/simon/MainActivity.java
+++ app/src/main/java/csci4020/shawnbickel_judsonthomas/assignment2/simon/MainActivity.java
@@ -1,7 +1,8 @@
 package csci4020.shawnbickel_judsonthomas.assignment2.simon;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.play_button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
     }
 }
