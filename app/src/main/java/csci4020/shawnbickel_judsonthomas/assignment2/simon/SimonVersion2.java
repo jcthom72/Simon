a1338d1f19558416fdb79850dd815c360c963d48
student456773 <sbickel201@gmail.com>
2017-02-28 14:50:51 -0600

    file updates

--- app/src/main/java/csci4020/shawnbickel_judsonthomas/assignment2/simon/SimonVersion2.java
+++ app/src/main/java/csci4020/shawnbickel_judsonthomas/assignment2/simon/SimonVersion2.java
@@ -1,13 +1,27 @@
 package csci4020.shawnbickel_judsonthomas.assignment2.simon;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class SimonVersion2 extends AppCompatActivity {
-
+    protected SimonGameEngine game2;
     private PlaySimon playSimonGame;
+    private final String HighScoreV2 = "HighScoreV2.txt";
+    protected SimonActivity.ButtonSequenceTask sequenceAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +30,6 @@ public class SimonVersion2 extends AppCompatActivity {
 
         // Initialize Game when user pushes the start button
         findViewById(R.id.play_button).setOnClickListener(new InitializeGame());
-        findViewById(R.id.pause_button).setOnClickListener(new PauseLevel());
     }
 
     class InitializeGame implements View.OnClickListener{
@@ -37,13 +50,68 @@ public class SimonVersion2 extends AppCompatActivity {
         }
     }
 
-    class PauseLevel implements View.OnClickListener{
+    protected void updateScoreText(){
+        ((TextView) findViewById(R.id.HighScore)).setText("" + game2.player.getScore());
+        int s = game2.player.getScore();
+        String score = Integer.toString(s);
+        saveHighScore(score);
+    }
 
-        @Override
-        public void onClick(View view) {
-            // implement logic to save the state of the level
+    // saves the player's score to a file
+    private void saveHighScore(String score)  {
+        try {
+            FileOutputStream fos = openFileOutput(HighScoreV2, Context.MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            BufferedWriter bw = new BufferedWriter(osw);
+            PrintWriter pw = new PrintWriter (bw);
+            pw.print(score);
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Can't write to file", Toast.LENGTH_LONG).show();
         }
     }
+
+    // returns a player's score from a file
+    public String returnHighScore(){
+        String score = "";
+
+        try {
+            FileInputStream fis = openFileInput(HighScoreV2);
+            Scanner s = new Scanner(fis);
+            while(s.hasNext()){
+                score = s.next();
+            }
+            s.close();
+        } catch (FileNotFoundException e) {
+            Log.i("ReadData", "no input file found");
+        }
+        return score;
+    }
+
+    protected void gameStartEvent(){
+        game2.startGame();
+        TextView score = (TextView) findViewById(R.id.HighScore);
+        // sets the player's score retrieved from file
+        String sc = returnHighScore();
+        score.setText(sc);
+
+        try{
+            int s = Integer.parseInt(sc);
+            game2.player.setScore(s);
+        }catch (NumberFormatException e){
+            updateScoreText();
+        }
+
+
+        updateScoreText();
+
+        if(sequenceAnim == null){
+            //sequenceAnim = new SimonActivity.ButtonSequenceTask();
+        }
+        //sequenceAnim.execute();
+    }
+
 
     // PlaySimon class includes methods to execute threads
     class PlaySimon extends AsyncTask<Void,Void,Void>{
--- app/src/main/java/csci4020/shawnbickel_judsonthomas/assignment2/simon/SimonVersion3.java
+++ app/src/main/java/csci4020/shawnbickel_judsonthomas/assignment2/simon/SimonVersion3.java
@@ -1,14 +1,27 @@
 package csci4020.shawnbickel_judsonthomas.assignment2.simon;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class SimonVersion3 extends AppCompatActivity {
-
+    protected SimonGameEngine game3;
     private PlaySimon playSimonGame;
-
+    private final String HighScoreV3 = "HighScoreV3.txt";
+    protected SimonActivity.ButtonSequenceTask sequenceAnim;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +29,7 @@ public class SimonVersion3 extends AppCompatActivity {
 
         // Initialize Game when user pushes the start button
         findViewById(R.id.play_button).setOnClickListener(new InitializeGame());
-        findViewById(R.id.pause_button).setOnClickListener(new PauseLevel());
+
     }
 
     class InitializeGame implements View.OnClickListener{
@@ -37,13 +50,68 @@ public class SimonVersion3 extends AppCompatActivity {
         }
     }
 
-    class PauseLevel implements View.OnClickListener{
+    protected void updateScoreText(){
+        ((TextView) findViewById(R.id.HighScore)).setText("" + game3.player.getScore());
+        int s = game3.player.getScore();
+        String score = Integer.toString(s);
+        saveHighScore(score);
+    }
 
-        @Override
-        public void onClick(View view) {
-            // implement logic to save the state of the level
+    // saves the player's score to a file
+    private void saveHighScore(String score)  {
+        try {
+            FileOutputStream fos = openFileOutput(HighScoreV3, Context.MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            BufferedWriter bw = new BufferedWriter(osw);
+            PrintWriter pw = new PrintWriter (bw);
+            pw.print(score);
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Can't write to file", Toast.LENGTH_LONG).show();
         }
     }
+
+    // returns a player's score from a file
+    public String returnHighScore(){
+        String score = "";
+
+        try {
+            FileInputStream fis = openFileInput(HighScoreV3);
+            Scanner s = new Scanner(fis);
+            while(s.hasNext()){
+                score = s.next();
+            }
+            s.close();
+        } catch (FileNotFoundException e) {
+            Log.i("ReadData", "no input file found");
+        }
+        return score;
+    }
+
+    protected void gameStartEvent(){
+        game3.startGame();
+        TextView score = (TextView) findViewById(R.id.HighScore);
+        // sets the player's score retrieved from file
+        String sc = returnHighScore();
+        score.setText(sc);
+
+        try{
+            int s = Integer.parseInt(sc);
+            game3.player.setScore(s);
+        }catch (NumberFormatException e){
+            updateScoreText();
+        }
+
+
+        updateScoreText();
+
+        if(sequenceAnim == null){
+            //sequenceAnim = new SimonActivity.ButtonSequenceTask();
+        }
+        //sequenceAnim.execute();
+    }
+
 
     // PlaySimon class includes methods to execute threads
     class PlaySimon extends AsyncTask<Void,Void,Void>{
